1d249292756fe8a9cfe86c4d647b3d24151d64e0
Celeste Martinez Chavez <celestemtz0903@gmail.com>
2025-08-25 11:10:04 -0600

    Segundo ejercicio

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,1 +1,10 @@
-ajjf
+#Creacion de una galleta
+
+class Galleta:
+    def __init__(self, nombre, forma):
+        self.nombre=nombre
+        self.forma=forma
+    
+    def hornear(self):
+        print("Esta",self.nombre,"ha sido horneada en forma de",self.forma)
+    
---
+++ ejercicio2.py
@@ -0,0 +1,9 @@
+class Libro:
+    def __init__(self, titulo, autor, precio):
+        self.titulo=titulo
+        self.autor=autor
+        self.precio=precio
+        
+    def mostrar_informacion(self):
+        print(f"El libro titulado'{self.titulo}', escrito por '{self.autor}' se vende a {self.precio}")
+        
